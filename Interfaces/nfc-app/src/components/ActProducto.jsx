0a7ca5dd12c20ebefb98ff1e30c5a9b63a7b5483
Juan Esteban Sanchez <90722320+JuanEsteban05@users.noreply.github.com>
2021-10-26 14:07:03 -0500

    Actualizar productos

--- Interfaces/nfc-app/src/components/ActProducto.jsx
+++ Interfaces/nfc-app/src/components/ActProducto.jsx
@@ -1,6 +1,63 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { useParams, useHistory } from 'react-router-dom'
+import { actualizarDocumentoDatabase,  consultarDocumentoDatabase } from './firebaseconf';
 
 export const ActProducto = () => {
+
+    const { id } = useParams()
+    console.log(id);
+    const [cantidad, setCantidad] = useState(0);
+    const [modelo, setModelo] = useState('');
+    const [descripcion, setDescripcion] = useState('');
+    const [precio, setPrecio] = useState(0);
+    const [fechaRegistro, setFechaRegistro] = useState('');
+    const [fechaVenta, setFechaVenta] = useState('');
+    const [estado, setEstado] = useState('');
+    const [codAgencia, setCodAgencia] = useState('');
+    const [color, setColor] = useState('');
+    const [motor, setMotor] = useState('');
+    const history = useHistory();
+
+    useEffect(async ()=>{
+        const productoTemp = await consultarDocumentoDatabase('lista-productos', id)
+
+                setModelo(productoTemp.modelo)
+                setCantidad(productoTemp.cantidad)
+                setDescripcion(productoTemp.descripcion)
+                setPrecio(productoTemp.precio)
+                setFechaRegistro(productoTemp.fechaRegistro)
+                setFechaVenta(productoTemp.fechaVenta)
+                setEstado(productoTemp.estado)
+                setCodAgencia(productoTemp.codAgencia)
+                setColor(productoTemp.color)
+                setMotor(productoTemp.motor)
+            }, []);
+
+
+
+    
+           const handleActualizarProducto = async (e) => {
+             e.preventDefault()
+    
+         const producto = {
+             cantidad,
+             modelo,
+             descripcion,
+             precio,
+             fechaRegistro,
+             fechaVenta: fechaVenta,
+             estado,
+             codAgencia,
+             color,
+             motor,
+         }
+
+    
+         actualizarDocumentoDatabase('lista-productos', id, producto)
+         history.push('/')
+    
+    
+       }
 
 
     
@@ -9,49 +66,111 @@ export const ActProducto = () => {
             <main>
                 <div className="card-body">
                     <div className="card-header bg-white rounded border">
-                        <h1 className="titulo text-center">Actualización de productos</h1>
+                        <h1 className="titulo text-center">Actualización de producto</h1>
                     </div>
                     <br />
-                    <h2 className="card-subtitle mb-2 text-muted text-center">N° Producto: Asd323fds</h2>
-                    <form action method="POST">
-                        <br />
-                        <label className="form-label col-8 fw-bold" htmlFor="Precio">Precio</label>
-                        <div className="input-group">
-                            <input className="form-control" type="input" placeholder="Ingrese el precio" disabled id="precio" />
-                            <button className="btn btn-outline-dark btn-lg"><img src="/Interfaces/Sources/edit2.png" alt="editar" /></button>
-                        </div>
-                        <br />
-                        <div className="form-group">
-                            <label className="form-label col-8 fw-bold" htmlFor="desc">Descripción</label>
-                            <div className="input-group mb-3">
-                                <textarea className="form-control" id="desc" disabled defaultValue={""} />
-                                <button className="btn btn-outline-dark btn-lg"><img src="/Interfaces/Sources/edit2.png" alt="editar" /></button>
+                    <form>
+                           
+                        <div className="w-100 d-flex pt-3">
+                            <div className="w-50 pe-3 form-group">
+                                <label className="form-label fw-bold" htmlFor="cantidad">Cantidad</label>
+                                <input className="form-control" type="number" placeholder="Ingrese cantidad" id="cantidad" value={cantidad}
+                                    onChange={(e) => {
+                                    setCantidad(e.target.value);
+                                    
+                                }} />
+                            </div>
+                            <div className="w-50 ps-3">
+                                <label className="form-label col-8 fw-bold" htmlFor="Precio">Precio</label>
+                                <input className="form-control" type="number" placeholder="Ingrese el precio" id="precio"  value={precio} 
+                                    onChange={(e) => {
+                                    setPrecio(e.target.value);
+                                   
+                                }} />
                             </div>
                         </div>
-                        <br />
-                        <label className="form-label col-8 fw-bold" htmlFor="model">Modelo</label>
-                        <div className="input-group mb-3">
-                            <select name="modelo" className="form-control" id="model" disabled>
-                                <option value="Mod_Default">Selecciona un Modelo</option>
-                                <option value="Sentra Sr">Sentra Sr</option>
-                                <option value="Mazda CX-9">Mazda CX-9</option>
-                                <option value="Chevrolet Equinox">Chevrolet Equinox</option>
-                            </select>
-                            <button className="btn btn-outline-dark btn-lg"><img src="/Interfaces/Sources/edit2.png" alt="editar" /></button>
+                        <div className="w-100 pt-3 d-flex">
+                            <div className="w-50 pe-3">
+                                <label className="form-label fw-bold" htmlFor="desc">Descripción</label>
+                                <div className="w-100">
+                                    <textarea className="form-control" id="desc"  value={descripcion}  onChange={(e) => {
+                                        setDescripcion(e.target.value);
+                                        
+                                    }} />
+                                </div>
+                            </div>
+
+                            <div className="w-50 ps-3">
+                                <label className="form-label fw-bold" htmlFor="model">Modelo</label>
+                                <select name="modelo" className="form-control form-label" id="model" value={modelo} onChange={(e) => {
+                                    setModelo(e.target.value);
+                                }}>
+                                    <option value="Mod_Default">Selecciona un Modelo</option>
+                                    <option value="Sentra Sr">Sentra Sr</option>
+                                    <option value="Mazda CX-9">Mazda CX-9</option>
+                                    <option value="Chevrolet Equinox">Chevrolet Equinox</option>
+                                </select>
+                            </div>
                         </div>
-                        <br />
-                        <label className="form-label col-8 fw-bold" htmlFor="state">Estado</label>
-                        <div className="input-group mb-3">
-                            <select className="form-control" id="state" disabled>
-                                <option value="Mod_Default">Seleccione el estado</option>
-                                <option value="disponible">Disponible</option>
-                                <option value="No_disponible">No disponible</option>
-                            </select>
-                            <button className="btn btn-outline-dark btn-lg"><img src="/Interfaces/Sources/edit2.png" alt="editar" /></button>
+
+                        <div className="w-100 pt-3 d-flex">
+                            <div className="w-50 pe-3">
+                                <label className="form-label fw-bold" htmlFor="dateReg">Fecha de Registro</label>
+                                <input className="form-control" type="datetime-local" id="dateReg" value={fechaRegistro} onChange={(e) => {
+                                    setFechaRegistro(e.target.value);
+                                }} />
+                            </div>
+                            <div className="w-50 ps-3">
+                                <label className="form-label fw-bold" htmlFor="dateVen">Fecha de Venta</label>
+                                <input className="form-control" type="date" placeholder="Ingrese fecha de venta" id="dateVen" value={fechaVenta}
+                                 onChange={(e) => {
+                                    setFechaVenta(e.target.value);
+                                }} />
+                            </div>
                         </div>
-                        <br />
-                        <div className="text-center ">
-                            <a href="/Interfaces/confirmacion-registrar-producto.html" role="button" className="btn btn-dark btn-lg">Actualizar</a>
+
+                        <div className="w-100 pt-3 d-flex">
+                            <div className="w-50 pe-3">
+                                <label className="form-label fw-bold" htmlFor="state">Estado</label>
+                                <select className="form-control" id="state" value={estado} onChange={(e) => {
+                                    setEstado(e.target.value);
+                                }}>
+                                    <option value="Mod_Default">Seleccione el estado</option>
+                                    <option value="disponible">Disponible</option>
+                                    <option value="No_disponible">No disponible</option>
+                                </select>
+                            </div>
+                            <div className="w-50 ps-3">
+                                <label className="form-label fw-bold" htmlFor="codAgencia">Codigo de Agencia</label>
+                                <input className="form-control" type="text" placeholder="codigo de agencia" value={codAgencia} id="codAgencia" onChange={(e) => {
+                                    setCodAgencia(e.target.value);
+                                }} />
+                            </div>
+                        </div>
+
+                        <div className="w-100 pt-3 d-flex">
+                            <div className="w-50 pe-3">
+                                <label className="form-label fw-bold" htmlFor="color">Color</label>
+                                <select className="form-control" id="color" value={color} onChange={(e) => {
+                                    setColor(e.target.value);
+                                }}>
+                                    <option value="Mod_Default">Seleccione Color</option>
+                                    <option value="Negro">Negro</option>
+                                    <option value="Blanco">Blanco</option>
+                                    <option value="Rojo">Rojo</option>
+                                    <option value="Azul">Azul</option>
+                                    <option value="Gris">Gris</option>
+                                </select>
+                            </div>
+                            <div className="w-50 ps-3">
+                                <label className="form-label fw-bold" htmlFor="motor">Modelo del Motor</label>
+                                <input className="form-control" type="text" placeholder="ingrese modelo" id="motor" value={motor}onChange={(e) => {
+                                    setMotor(e.target.value);
+                                }} />
+                            </div>
+                        </div>
+                        <div className="text-center pt-3 pb-3 ">
+                            <button className="btn btn-dark btn-lg" onClick={handleActualizarProducto}>Actualizar</button>
                         </div>
                     </form>
                 </div>
@@ -61,4 +180,4 @@ export const ActProducto = () => {
 
         </>
     )
-}
+                            }
